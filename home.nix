0b7df8da001bd89986afd64228c13ce304e6c0a6
Paul Fauchon <paulfauchon@xendit.co>
2023-04-25 15:24:53 +0800

    fleek: apply

--- home.nix
+++ home.nix
@@ -33,6 +33,16 @@
     pkgs.htop
     pkgs.github-cli
     pkgs.glab
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.vscode
+    pkgs.lazygit
+    pkgs.jq
+    pkgs.yq
+    pkgs.neovim
+    pkgs.neofetch
+    pkgs.btop
+    pkgs.cheat
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
   fonts.fontconfig.enable = true; 
--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
-{ pkgs, misc, ... }: {# zsh
+{ pkgs, misc, ... }: {
+    programs.exa.enableAliases = true;
+    
+    programs.exa.extraOptions = [
+   "--group-directories-first"
+   "--header"
+];
+    
+    programs.bat.config = {
+  theme = "TwoDark";
+};
+    # zsh
   programs.zsh.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
