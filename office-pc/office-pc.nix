dc2eddf721a42b72186a49b72fb17e3858d84a0a
gz <paul@fauchon.dev>
2023-04-26 21:47:22 +0800

    fleek: new system

---
+++ office-pc/office-pc.nix
@@ -0,0 +1,27 @@
+{ pkgs, misc, ... }: {
+    home.username = "gz";
+    home.homeDirectory = "/home/gz";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "gz";
+        userEmail = "paul@fauchon.dev";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "~/.ssh/id_rsa";
+            signByDefault = builtins.stringLength "~/.ssh/id_rsa" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+}
