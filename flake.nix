9455ce5c8a0dbb735ec2dbd13d734c4a5abb8099
Paul Fauchon <paul@fauchon.dev>
2023-04-26 23:33:46 +0800

    fleek: apply

--- flake.nix
+++ flake.nix
@@ -8,13 +8,9 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Fleek
-    fleek.url = "github:ublue-os/fleek";
-
   };
 
-  outputs = { nixpkgs, home-manager, fleek, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
 
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
@@ -22,7 +18,6 @@
       "xendit@yoga7" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-
         modules = [ 
           ./home.nix 
           ./path.nix
@@ -33,20 +28,12 @@
           # Host Specific configs
           ./yoga7/yoga7.nix
           ./yoga7/user.nix
-          # self-manage fleek
-          {
-           home.packages = [
-            fleek.packages.x86_64-linux.default
-          ];
-          }
-
         ];
       };
       
       "gz@yoga7" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-
         modules = [ 
           ./home.nix 
           ./path.nix
@@ -57,37 +44,6 @@
           # Host Specific configs
           ./yoga7/yoga7.nix
           ./yoga7/user.nix
-          # self-manage fleek
-          {
-           home.packages = [
-            fleek.packages.x86_64-linux.default
-          ];
-          }
-
-        ];
-      };
-      
-      "gz@office-pc" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-
-        modules = [ 
-          ./home.nix 
-          ./path.nix
-          ./shell.nix
-          ./user.nix
-          ./aliases.nix
-          ./programs.nix
-          # Host Specific configs
-          ./office-pc/office-pc.nix
-          ./office-pc/user.nix
-          # self-manage fleek
-          {
-           home.packages = [
-            fleek.packages.x86_64-linux.default
-          ];
-          }
-
         ];
       };
       
