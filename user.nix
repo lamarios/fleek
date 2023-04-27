e3b089058d86b8ec2711ec2107c3ec13dcea91a0
Paul Fauchon <paulfauchon@xendit.co>
2023-04-27 16:06:45 +0800

    fleek: apply

--- user.nix
+++ user.nix
@@ -2,7 +2,8 @@
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
   # home manager options available here: https://nix-community.github.io/home-manager/options.html
-  
+ 
+  nixpkgs.config.allowUnfree = true; 
   programs.zsh.enableAutosuggestions=true;
 
   programs.zsh.sessionVariables = {
