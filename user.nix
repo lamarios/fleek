6bb824c63e59c0655df2236fd6bb6ab9bcb0eded
Paul Fauchon <paulfauchon@xendit.co>
2023-05-02 19:14:21 +0700

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,4 +1,4 @@
-{ pkgs, misc, config.. }: {
+{ pkgs, misc, config, ... }: {
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
   # home manager options available here: https://nix-community.github.io/home-manager/options.html
