92634c78ffd7905c3f0057d2d2279064d5b64852
Paul Fauchon <paulfauchon@xendit.co>
2023-05-08 14:57:05 +0800

    fleek: apply

--- programs.nix
+++ programs.nix
@@ -9,7 +9,6 @@
 
   # User specified programs 
     programs.dircolors.enable = true; 
-    programs.zsh.oh-my-zsh.enable = true; 
-    programs.firefox.enable = true;
+    programs.zsh.oh-my-zsh.enable = true;
 
 }
