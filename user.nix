5da75f5714d04a2abca0c3dedff54d7aac50daa4
Paul Fauchon <paulfauchon@xendit.co>
2023-05-08 14:38:51 +0800

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, config, lib, ... }:
 let
   # ...
-  # ...
+  nixgl = import <nixgl> {} ;
   nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
     mkdir $out
     ln -s ${pkg}/* $out
@@ -9,7 +9,7 @@ let
     mkdir $out/bin
     for bin in ${pkg}/bin/*; do
      wrapped_bin=$out/bin/$(basename $bin)
-     echo "exec ${lib.getExe pkgs.nixgl.nixGLDefault} $bin \$@" > $wrapped_bin
+     echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
      chmod +x $wrapped_bin
     done
   '';
