f7132ed9e5176fb5d4fc2a136fbe8e2b34e51bdf
Paul Fauchon <paulfauchon@xendit.co>
2023-05-17 11:25:36 +0800

    fleek: apply

--- path.nix
+++ path.nix
@@ -3,5 +3,6 @@
  home.sessionPath = [ 
     "$HOME/bin"
     "$HOME/.local/bin"
+    "$HOME/.npm-packages/bin"
  ];
 }
--- user.nix
+++ user.nix
@@ -25,6 +25,7 @@ in {
 
   programs.zsh.sessionVariables = {
    ZSH_DISABLE_COMPFIX="true";
+   NODE_PATH="$HOME/.npm-packages/lib/node_modules"
   };
 
 
